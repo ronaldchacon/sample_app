e59aafc530005d8ec45b4458ae4e69c092165a76
Ronald Chacon <ronald.chacon87@gmail.com>
2015-04-01 22:26:56 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
